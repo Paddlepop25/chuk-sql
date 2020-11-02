496279661062f3ab3263b022c689f517f57a9626
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-11-03 00:34:46 +0800

    added pagination

--- mainTest.js
+++ mainTest.js
@@ -8,7 +8,9 @@ const mysql = require('mysql2/promise')
 // never use string concatenation in SQL query
 // one statement no need semi colon. multi statement then need
 // SQL driver only do one at a time. If want multiple statements need to turn on. We should do one at a time
-const SQL_FIND_BY_NAME = 'select * from apps where name like ? limit ?'
+const SQL_FIND_BY_NAME = 'select * from apps where name like ? limit ?,?'
+const SQL_RESULT_COUNT = 'select count(*) from apps where name like ?'
+
 // const SQL_NUMBER_OF_SEARCHES = 'select COUNT(*) from apps where appid like ? limit ?'
 
 // configure PORT
@@ -22,7 +24,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 4,
-  timezone: '+8:00'
+  timezone: '+8:00',
 })
 
 const startApp = async (app, pool) => {
@@ -36,7 +38,7 @@ const startApp = async (app, pool) => {
     // release the connection
     conn.release()
 
-    // start the server
+    // start the server only if connected to database
     app.listen(PORT, () => {
       console.info(`Application started on port ${PORT} at ${new Date()}`)
     })
@@ -61,9 +63,22 @@ app.get('/',
     res.render('index')
 })
 
+let offset = 0
+let limit = 5
 app.get('/search', 
   async (req, res) => {
     const q = req.query['search']
+    const minusOffset = req.query['previous'] //querying 'previous' button with name='previous' and value will be returned
+    // console.info('minusOffset --->', minusOffset)
+    const addOffset = req.query['next'] //querying 'next' button with name='next and value will be returned
+    // console.info('addOffset --->', addOffset)
+
+    if (minusOffset === 'previous5') {
+      offset -= 5
+    } else if (addOffset === 'next5' ) {
+      offset += 5
+    }
+    
     // acquire a connection from the pool
     const conn = await pool.getConnection()
     // console.info(q) // search
@@ -73,15 +88,27 @@ app.get('/search',
       // 2nd parameter is array of the ? above
       // query returns a promise. return an array of 2 elements  
       // 1st element will hold array of 10 records. the 2nd element is the metadata on the record, usually we don't need to look at it. so result[0]
-      const result = await conn.query(SQL_FIND_BY_NAME, [ `%${q}%`, 50 ])
+      if (offset < 0) {
+        offset = 0
+      }
+      const result = await conn.query(SQL_FIND_BY_NAME, [ `%${q}%`, offset,limit ])
+      const resultCount = await conn.query(SQL_RESULT_COUNT, [ `%${q}%` ])
       const recs = result[0]
-      // const recs = ""
+      const count = resultCount[0]
+      // console.info('count --->', count.filter(d => Object.values(d))) // [ TextRow { 'count(*)': 1 } ]
+      const recsLength = recs.length > 0
       // console.info('result ---->', result)
       // console.info('recs ---->', recs)
+      
+      if (recs.length < limit) {
+        offset = recs.length
+        console.info('rec.length--->', recs.length)
+      }
 
       res.status(200)
       res.type('text/html')
-      res.render('results', { recs })
+      res.render('results', { q, recs, recsLength })
+
     } catch(err) {
       console.error('error ---->', err)
     } finally {
